59b80d58b6048217cdaeccf1e65cd8ac3b57951b
Anubhav <anubhavv909@gmail.com>
2023-02-24 20:31:41 +0530

    Updated

--- HelpingHand/Users/models.py
+++ HelpingHand/Users/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class UserData(models.Model):
     id = models.AutoField
     name = models.CharField(max_length=30)
-    phone = models.BigIntegerField(max_length=10)
+    phone = models.IntegerField()
     email = models.EmailField(max_length = 254)
     password = models.CharField(max_length = 30)
 
